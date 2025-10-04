cd769e78692c1f560a926e02b3dc0007ed17d667
jonatas <jonataseduardo@gmail.com>
2025-10-04 15:26:46 -0300

    refact: remove unused plugings

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -51,13 +51,6 @@ require('lazy').setup({
 
   require 'kickstart/plugins/chatgpt',
 
-  require 'kickstart/plugins/avante',
-
-  require 'kickstart/plugins/no-neck-pain',
-
-  require 'kickstart/plugins/dirty-talk',
-
-  require 'kickstart/plugins/barbecue',
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
